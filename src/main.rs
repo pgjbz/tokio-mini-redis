693fa672fd9a15e04d6c52a8c6378ce3e17c97a8
Paulo Gabriel Justino Bezerra <pgjbzr@gmail.com>
2022-06-13 18:10:01 -0300

    use tokio mutex

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,21 @@
-use std::{
-    collections::HashMap,
-    net::SocketAddr,
-    sync::{Arc, Mutex},
-};
+use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 
 use bytes::Bytes;
 use mini_redis::{Command, Connection, Frame, Result};
 
-use tokio::net::{TcpListener, TcpStream};
+use tokio::{
+    net::{TcpListener, TcpStream},
+    sync::Mutex,
+};
 
 type Db = Arc<Mutex<HashMap<String, Bytes>>>;
-
-#[derive(Default, Clone)]
-struct Database {
-    db: Db,
-}
-
-impl Database {
-    fn insert(&self, key: String, value: Bytes) {
-        let mut db = self.db.lock().unwrap();
-        db.insert(key, value);
-    }
-
-    fn get(&self, key: &str) -> Option<Bytes> {
-        let db = self.db.lock().unwrap();
-        db.get(key).cloned()
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let addr: SocketAddr = "0.0.0.0:6379".parse()?;
 
     let listener = TcpListener::bind(addr).await?;
-    let db: Database = Database::default();
+    let db: Db = Db::default();
     loop {
         match listener.accept().await {
             Ok((stream, _)) => {
@@ -43,27 +25,30 @@ async fn main() -> Result<()> {
                     source: https://tokio.rs/tokio/tutorial/spawning
                 */
                 let db = db.clone();
-                tokio::spawn(async move { process(stream, &db).await });
+                tokio::spawn(async move { process(stream, db).await });
             }
             Err(e) => eprintln!("error on handle conntction {}", e),
         }
     }
 }
 
-async fn process(stream: TcpStream, db: &Database) {
+async fn process(stream: TcpStream, db: Db) {
     let mut connection = Connection::new(stream);
     while let Ok(Some(frame)) = connection.read_frame().await {
         println!("GOT: {:?}", frame);
         let response = match Command::from_frame(frame).unwrap() {
             Command::Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().clone());
+                let mut lock = db.lock().await;
+                lock.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_owned())
             }
-            Command::Get(cmd) => db
-                .get(cmd.key())
-                .map(Frame::Bulk)
-                .or_else(|| Some(Frame::Simple("nil".to_owned())))
-                .unwrap(),
+            Command::Get(cmd) => {
+                let lock = db.lock().await;
+                lock.get(cmd.key())
+                    .map(|val| Frame::Bulk(val.clone()))
+                    .or_else(|| Some(Frame::Simple("nil".to_owned())))
+                    .unwrap()
+            }
             _ => Frame::Error("unimplemented".to_string()),
         };
 
