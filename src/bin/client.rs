57cc831af56678e69fde9e29bc2777bb2b4567cd
Paulo Gabriel Justino Bezerra <pgjbzr@gmail.com>
2022-06-20 09:57:26 -0300

    add client and server bins
    Use channels to send message between tasks

---
+++ src/bin/client.rs
@@ -0,0 +1,67 @@
+use bytes::Bytes;
+use mini_redis::client;
+use tokio::sync::{mpsc, oneshot};
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
+
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+        resp: Responder<()>,
+    },
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
+
+    let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Get {
+            key: "foo".to_string(),
+            resp: resp_tx,
+        };
+        tx.send(cmd).await.unwrap();
+        let resp = resp_rx.await;
+        println!("GOT = {:?}", resp);
+    });
+
+    let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+            resp: resp_tx,
+        };
+        tx2.send(cmd).await.unwrap();
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
+
+    let manager = tokio::spawn(async move {
+        use Command::*;
+        let mut client = client::connect("0.0.0.0:6379").await.unwrap();
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Get { key, resp } => {
+                    let res = client.get(&key).await;
+                    let _ = resp.send(res);
+                }
+                Set { key, val, resp } => {
+                    let res = client.set(&key, val).await;
+                    let _ = resp.send(res);
+                }
+            }
+        }
+    });
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
+}
